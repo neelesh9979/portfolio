63447c7c53b23003e2b94e735b17214472fdffdc
Neelesh Sahu <neelesh.sahu@udaantechnologies.info>
2023-10-20 17:20:25 +0530

    Design folder

---
+++ port-html/app.js
@@ -0,0 +1,14 @@
+(function () {
+    console.log('my console',document.querySelectorAll(".control"));
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
