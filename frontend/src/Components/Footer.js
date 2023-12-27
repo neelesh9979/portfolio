b16cc8a1b1f10c78b9cf8562cb299d9943d4f57e
Neelesh Sahu <113585389+neelesh9979@users.noreply.github.com>
2023-12-27 18:21:54 +0530

    Delete frontend directory

--- frontend/src/Components/Footer.js
+++
@@ -1,5 +0,0 @@
-export default function Footer(){
-    return(
-        <h3>Footer Section</h3>
-    );
-}
--- frontend/src/Components/Header.js
+++
@@ -1,5 +0,0 @@
-export default function Header() {
-    return(
-        <h3>Header section</h3>
-    );
-}
